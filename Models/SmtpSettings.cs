de1aa6cd9c9c3b9ae7e1ee99f82c774b7e530e64
Cezol-ZaMan <phymatcom@gmail.com>
2024-10-25 11:32:29 +0200

    add model for smtp settings

---
+++ Models/SmtpSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace blazorTickets.Models
+{
+    public class SmtpSettings
+    {
+        public required string Host { get; set; }
+        public required int Port { get; set; }
+        public required bool EnableSsl { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+    }
+}
