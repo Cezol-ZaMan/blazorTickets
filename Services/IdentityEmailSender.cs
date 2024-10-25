a3786a95e4cd9ad093b36fe1aaae61825ff958b9
Cezol-ZaMan <phymatcom@gmail.com>
2024-10-25 11:59:30 +0200

    add service for account related emails

---
+++ Services/IdentityEmailSender.cs
@@ -0,0 +1,31 @@
+﻿using blazorTickets.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace blazorTickets.Services
+{
+    public class IdentityEmailSender : IEmailSender<ApplicationUser>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public IdentityEmailSender(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+        {
+            return _emailSender.SendEmailAsync(email, "Confirm your email", $"Please confirm your account by <a href='{confirmationLink}'>clicking here</a>.");
+        }
+
+        public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+        {
+            return _emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by <a href='{resetLink}'>clicking here</a>.");
+        }
+
+        public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+        {
+            return _emailSender.SendEmailAsync(email, "Reset your password", $"Please reset your password by using the following code {resetCode}");
+        }
+    }
+}
